802bf56211f1ac5e325a428c0044811819e85a95
derricks <derrick.schneider@gmail.com>
2020-03-14 19:54:06 -0700

    Create initial puzzle_helper command using cobra
    The first sub command does frequency analysis for cryptograms

---
+++ cmd/cryptogram.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// cryptogramCmd represents the cryptogram command
+var cryptogramCmd = &cobra.Command{
+	Use:   "cryptogram",
+	Short: "Provides a variety of tools for doing cryptanalyis",
+	Long: `The cryptogram subcommand is designed to help with puzzle-level cryptanalysis.
+
+	Examples:
+	   puzzles cryptogram freq `,
+}
+
+var freqCmd = &cobra.Command{
+	Use:   "freq",
+	Short: "Provides frequency information for the uppercase letters in a string.",
+	Long:  `Many common cryptograms require frequency analysis. This command provides single-character frequency as well as digraphs and trigraphs.`,
+	Args:  cobra.MinimumNArgs(1),
+	Run:   printFrequencyTable,
+}
+
+func init() {
+	cryptogramCmd.AddCommand(freqCmd)
+	rootCmd.AddCommand(cryptogramCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cryptogramCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cryptogramCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// printFrequencyTable generates output about the frequency of characters, digraphs, and trigraphs in a string
+func printFrequencyTable(cmd *cobra.Command, args []string) {
+	totalString := strings.Join(args, " ")
+	singleLetterCounts := frequencyCountInString(totalString)
+	totalLetterCount := countTotalCharacters(totalString)
+	fmt.Println("Frequency Table")
+	fmt.Println("---------------")
+	fmt.Printf("Total letters: %v\n", totalLetterCount)
+	for curByte, count := range singleLetterCounts {
+		fmt.Printf("%c: %v (%v%%)\n", curByte, count, fmt.Sprintf("%.2f", 100.0*(float32(count)/float32(totalLetterCount))))
+	}
+}
+
+// countTotalCharacters counts the number of uppercase letters in the given string
+func countTotalCharacters(toCount string) int {
+	var totalCount = 0
+	for _, curByte := range []byte(toCount) {
+		if isUppercaseAscii(curByte) {
+			totalCount += 1
+		}
+	}
+	return totalCount
+}
+
+// frequencyCountInString takes in a string and returns the frequency of bytes in it
+func frequencyCountInString(toCount string) map[byte]int {
+	counts := make(map[byte]int)
+	for _, curByte := range []byte(toCount) {
+		if !isUppercaseAscii(curByte) {
+			continue
+		}
+
+		curCount, exists := counts[curByte]
+		if exists {
+			counts[curByte] = curCount + 1
+		} else {
+			counts[curByte] = 1
+		}
+	}
+	return counts
+}
+
+func isUppercaseAscii(check byte) bool {
+	return check >= 65 && check < 91
+}
---
+++ cmd/cryptogram_test.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestIsUppercaseAscii(test *testing.T) {
+	expectedResults := map[byte]bool{
+		'A': true,
+		'Z': true,
+		'M': true,
+		'a': false,
+		'[': false,
+		'@': false,
+	}
+
+	for toTest, expected := range expectedResults {
+		if isUppercaseAscii(toTest) != expected {
+			test.Errorf("Expected isUppercaseAscii(%c) to return %v but returned %v instead", toTest, expected, isUppercaseAscii(toTest))
+		}
+	}
+}
+
+func TestFrequencyCountsInString(test *testing.T) {
+	tests := map[string]map[byte]int{
+		"D'M D'LL": map[byte]int{'D': 2, 'M': 1, 'L': 2, byte(39): 0, ' ': 0},
+	}
+
+	for testString, expectedResults := range tests {
+		freqs := frequencyCountInString(testString)
+
+		for curByte, expectedCount := range expectedResults {
+			actualCount, found := freqs[curByte]
+			if found && (actualCount != expectedCount) {
+				test.Errorf("Expected %v for %c but got %v", expectedCount, curByte, actualCount)
+			}
+
+			if !found && expectedCount != 0 {
+				test.Errorf("For %c expected count of %v but got count of %v", curByte, expectedCount, actualCount)
+			}
+		}
+	}
+}
+
+func TestCountTotalCharacters(test *testing.T) {
+	tests := map[string]int{
+		"D'M D'LL": 5,
+		"%$a'":     0,
+	}
+
+	for curString, expectedCount := range tests {
+		actualCount := countTotalCharacters(curString)
+		if expectedCount != actualCount {
+			test.Errorf("Expected %v for total characters in %s but got %v", expectedCount, curString, actualCount)
+		}
+	}
+}
